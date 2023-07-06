2449d1bd1b7614d0a36f68e2a007d22bf1533fa3
Surya B <gayashiva91@gmail.com>
2023-07-06 18:29:00 +0530

    Tried slope layer

--- reduce_shp.py
+++ reduce_shp.py
@@ -22,7 +22,7 @@ def reduce_shapefile(original_shapefile, reduced_shapefile, chunk_size):
             # Iterate over the shapefile records in chunks
             for i, feature in enumerate(src):
                 # Process the feature and filter rows where ORD_FLOW equals 10
-                if feature['properties']['ORD_FLOW'] == 10:
+                if feature['properties']['Temp_min'] <= 0:
                     reduced_data.append(feature)
 
                 # Check if the chunk size has been reached
@@ -47,45 +47,14 @@ def reduce_shapefile(original_shapefile, reduced_shapefile, chunk_size):
         progress_bar.close()
 
 # Set the paths for the original and reduced shapefiles
-original_shapefile = 'data/HydroRIVERS_v10_shp/HydroRIVERS_v10_shp/HydroRIVERS_v10.shp'
-reduced_shapefile = 'data/HydroRIVERS_v10_shp/reduced_HydroRIVERS_v10.shp'
+# original_shapefile = 'data/HydroRIVERS_v10_shp/HydroRIVERS_v10_shp/HydroRIVERS_v10.shp'
+# reduced_shapefile = 'data/HydroRIVERS_v10_shp/reduced_HydroRIVERS_v10.shp'
+
+original_shapefile = 'data/GloRiC_v10_shapefile/GloRiC_v10_shapefile/GloRiC_v10.shp'
+reduced_shapefile = 'data/GloRiC_v10_shapefile/reduced_GloRiC_v10.shp'
 
 # Set the chunk size (adjust this based on your available memory)
 chunk_size = 10000
 
 # Call the function to reduce the shapefile in chunks
 reduce_shapefile(original_shapefile, reduced_shapefile, chunk_size)
-
-# import geopandas as gpd
-
-# # def reduce_shapefile(original_shapefile, reduced_shapefile):
-# #     # Read the original shapefile
-# #     data = gpd.read_file(original_shapefile)
-# #     
-# #     # Select the desired columns (geometry and ORD_FLOW)
-# #     columns_to_keep = ['ORD_FLOW', 'DIS_AV_CMS', 'geometry']
-# #     reduced_data = data[columns_to_keep]
-# #     
-# #     # Save the reduced data to a new shapefile
-# #     reduced_data.to_file(reduced_shapefile)
-
-# def reduce_shapefile(original_shapefile, reduced_shapefile):
-#     # Read the original shapefile
-#     data = gpd.read_file(original_shapefile)
-
-#     # Select the desired columns (geometry and ORD_FLOW)
-#     columns_to_keep = ['ORD_FLOW', 'DIS_AV_CMS', 'geometry']
-#     reduced_data = data[columns_to_keep]
-#     
-#     # Filter rows where ORD_FLOW equals 10
-#     reduced_data = reduced_data[reduced_data['ORD_FLOW'] == 10]
-#     
-#     # Save the reduced data to a new shapefile
-#     reduced_data.to_file(reduced_shapefile)
-
-# # Set the paths for the original and reduced shapefiles
-# original_shapefile = 'data/HydroRIVERS_v10_shp/HydroRIVERS_v10_shp/HydroRIVERS_v10.shp'
-# reduced_shapefile = 'data/HydroRIVERS_v10_shp/reduced_HydroRIVERS_v10.shp'
-
-# # Call the function to reduce the shapefile
-# reduce_shapefile(original_shapefile, reduced_shapefile)
